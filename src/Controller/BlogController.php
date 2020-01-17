53c597762b3c4a97858b9f491127bc9506705b08
kamel-yhf <kamelyakhlef@hotmail.fr>
2020-01-17 09:33:34 +0100

    backup

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -13,7 +13,7 @@ class BlogController extends AbstractController
      */
     public function home(){
         return $this->render('blog/home.html.twig',[
-            'title' => 'Bienvenue kamel', 'age' => 58,
+            'title' => 'Bienvenue kamel',
         ]);
     }
     /**
